4cc35e198733508219f7d19a97ec9d217701792f
Tushar Negi <negitushar81@gmail.com>
2024-09-20 11:06:22 +0530

    4. Loops

--- script.js
+++ script.js
@@ -4,23 +4,23 @@
 // Declare a variable to store your name and log it to the console.
 // How do you declare a constant variable in JavaScript? Show an example.
 
-const pi = 3.14;     // const and let was introduced in ES6
-let yName = "Your Name";
-var age = 32;   // used in old version of JS
+// const pi = 3.14;     // const and let was introduced in ES6
+// let yName = "Your Name";
+// var age = 32;   // used in old version of JS
 
-console.log(pi);
-console.log(yName);
-console.log(age);
+// console.log(pi);
+// console.log(yName);
+// console.log(age);
 
 
 
 // 2. Basic Operators
 // Write a JavaScript code to add two numbers and display the result.
 
-let a = 20;
-let b = 30
-let add = a + b;
-console.log(`a + b = ${add}`);
+// let a = 20;
+// let b = 30
+// let add = a + b;
+// console.log(`a + b = ${add}`);
 // console.log(20 + 30);  // direct method to add two numbers
 
 
@@ -28,10 +28,30 @@ console.log(`a + b = ${add}`);
 // 3. Conditional Statements
 // Write a program that checks if a person is adult or a minor using there age.
 
-let your_age = 20;
+// let your_age = 20;
 
-if (your_age >= 18) {
-    console.log("You're an adult");
-} else {
-    console.log("You're a minor");
+// if (your_age >= 18) {
+//     console.log("You're an adult");
+// } else {
+//     console.log("You're a minor");
+// }
+
+
+
+// 4. Loops
+// Write a for loop that prints numbers from 1 to 10.
+// How would you use a while loop to sum numbers from 1 to 100?
+
+// console.log("Counting 1 to 10 using for loop");
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+// console.log("Stopped");
+
+console.log("Counting 1 to 10 using while loop");
+let i = 1
+while(i <= 10){
+    console.log(i);
+    i++;
 }
+console.log("Stopped");
