a4f92a4c492fb4362affd392e493880f76107e16
Tushar Negi <negitushar81@gmail.com>
2024-09-20 10:53:56 +0530

    1. Variables and Data Types

---
+++ script.js
@@ -0,0 +1,13 @@
+// Beginner Level Questions -------------------------------------------------------
+
+// 1. Variables and Data Types
+// Declare a variable to store your name and log it to the console.
+// How do you declare a constant variable in JavaScript? Show an example.
+
+const pi = 3.14     // const and let was introduced in ES6
+let yName = "Your Name"
+var language = "Javascript"   // used in old version of JS
+
+console.log(pi);
+console.log(yName);
+console.log(language);
