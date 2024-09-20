619acaf55632d4439ce24ddc8abbe01dc6f42067
Tushar Negi <negitushar81@gmail.com>
2024-09-20 10:58:29 +0530

    2. Basic Operators

--- script.js
+++ script.js
@@ -4,10 +4,26 @@
 // Declare a variable to store your name and log it to the console.
 // How do you declare a constant variable in JavaScript? Show an example.
 
-const pi = 3.14     // const and let was introduced in ES6
-let yName = "Your Name"
-var age = 32   // used in old version of JS
+const pi = 3.14;     // const and let was introduced in ES6
+let yName = "Your Name";
+var age = 32;   // used in old version of JS
 
 console.log(pi);
 console.log(yName);
 console.log(age);
+
+
+
+// 2. Basic Operators
+// Write a JavaScript code to add two numbers and display the result.
+
+let a = 20;
+let b = 30
+let add = a + b;
+console.log(`a + b = ${add}`);
+// console.log(20 + 30);  // direct method to add two numbers
+
+
+
+// 3. Conditional Statements
+// Write a program that checks if a person is adult or a minor using there age.
